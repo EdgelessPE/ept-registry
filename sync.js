688e5595195861294aafd881460514a900ca38ad
Oxygen <3038094028@qq.com>
2021-05-16 14:44:02 +0800

    Update sync.js

--- sync.js
+++ sync.js
@@ -3,89 +3,132 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.worker = void 0;
-const axios_1 = __importDefault(require("axios"));
-const fs_extra_1 = __importDefault(require("fs-extra"));
+const koa_1 = __importDefault(require("koa"));
 const path_1 = __importDefault(require("path"));
-const worker_threads_1 = require("worker_threads");
-let pp = "https://zfile.edgeless.top/api/list/2?path=";
-if (!fs_extra_1.default.pathExistsSync("./mirrors"))
-    fs_extra_1.default.mkdirSync("./mirrors");
-async function er(e) {
-    if (e.type == "FILE") {
-        if (!fs_extra_1.default.pathExistsSync(`./${e.path}`))
-            fs_extra_1.default.mkdirSync(`./${e.path}`);
-        new worker_threads_1.Worker(__filename, {
-            workerData: ["", e]
-        });
+const fs_extra_1 = __importDefault(require("fs-extra"));
+const app = new koa_1.default();
+app.use(async (ctx, next) => {
+    let parsedPath = path_1.default.parse(decodeURIComponent(ctx.URL.pathname));
+    console.log(parsedPath);
+    if (parsedPath.dir == "/@api") {
+        next();
+        return;
     }
-    else if (e.type == "FOLDER") {
-        if (!fs_extra_1.default.pathExistsSync("./"+ e.path + e.name))
-            fs_extra_1.default.mkdirSync("./" + e.path + e.name);
-        await Sleep(1000);
-        let indexe = await axios_1.default.get(pp + encodeURIComponent(e.path + e.name));
-        if (indexe.data.code != 0)
-            throw "Err";
-        let cure = indexe.data.data.files;
-        cure.forEach(er);
+    if (parsedPath.dir == "/@browse") {
+        next();
+        return;
     }
-}
-(async () => {
-    if (worker_threads_1.isMainThread) {
-        //https://zfile.edgeless.top/api/list/2?path=%2F&password=&orderBy=&orderDirection=
-        let index = await axios_1.default.get(pp + encodeURIComponent("/"), {
-            headers: {
-                referer: "https://zfile.edgeless.top/",
-                "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36 Edg/90.0.818.62"
-            }
-        });
-        //console.log(pp+encodeURIComponent("/"), );
-        //fs.writeFile("./out.txt", JSON.stringify(index.data, null, 4));
-        if (index.data.code != 0)
-            throw "Err";
-        let cont = index.data.data.files;
-        cont.forEach(er);
+    let rpath = path_1.default.join("./mirrors/" + decodeURIComponent(ctx.URL.pathname));
+    let realPath = path_1.default.parse(path_1.default.join("./mirrors/" + decodeURIComponent(ctx.URL.pathname)));
+    if (fs_extra_1.default.existsSync(rpath) && (realPath.ext != '' || ctx.URL.searchParams.get("type") == "file")) {
+        ctx.status = 200;
+        let stream = fs_extra_1.default.createReadStream(rpath);
+        ctx.body = stream;
+    }
+    else if (fs_extra_1.default.pathExistsSync(rpath), (realPath.ext == '' || ctx.URL.searchParams.get("type") == "folder")) {
+        ctx.status = 403;
+        ctx.body = {
+            status: 403,
+            msg: "this is a folder"
+        };
     }
     else {
-        const data = worker_threads_1.workerData;
-        worker(data[0], data[1]);
+        ctx.status = 404;
+        ctx.body = {
+            status: 404,
+            msg: "not found"
+        };
     }
-})();
-async function worker(cur, uu) {
-    if (!fs_extra_1.default.pathExistsSync(`./${uu.path}`))
-        fs_extra_1.default.mkdirSync(`./${uu.path}`);
-    console.log("worker", uu, "start");
-    console.log(!fs_extra_1.default.pathExistsSync(`./${uu.path}`));
-    let ccc;
-    try {
-        ccc = await axios_1.default.get(encodeURI(uu.url), {
-            responseType: 'stream'
-        });
+});
+app.use(async (ctx, next) => {
+    let parsedPath = path_1.default.parse(decodeURIComponent(ctx.URL.pathname));
+    console.log(parsedPath);
+    if (parsedPath.dir == "/@browse") {
+        next();
+        return;
     }
-    catch { }
-    ;
-    let ccd = ccc.data;
-    let length = Math.max();
-    if (ccd.headers['content-length'] != undefined) {
-        length = parseInt(ccd.headers['content-length'], 10);
+    if (parsedPath.dir == "/@api") {
+        switch (parsedPath.base) {
+            case "dir":
+                let realPath = path_1.default.join("./mirrors/" + decodeURIComponent(ctx.URL.searchParams.get("path") ?? "./"));
+                if (fs_extra_1.default.pathExistsSync(realPath)) {
+                    let realPath_ = path_1.default.parse(realPath);
+                    console.log(realPath_);
+                    let a;
+                    if (realPath_.ext == '') {
+                        a = fs_extra_1.default.readdirSync(realPath, {
+                            encoding: "utf8",
+                            withFileTypes: true
+                        });
+                    }
+                    else {
+                        a = [{
+                                name: realPath_.name + realPath_.ext,
+                                __o: fs_extra_1.default.statSync(realPath),
+                            }];
+                    }
+                    console.log(a);
+                    ctx.body = JSON.stringify({
+                        status: 200,
+                        data: a.map(v => {
+                            let stat;
+                            if (v.__o) {
+                                stat = v.__o;
+                                v.isFile = () => v.__o.isFile();
+                                v.isDirectory = () => v.__o.isDirectory();
+                            }
+                            else {
+                                stat = fs_extra_1.default.statSync(path_1.default.join(realPath, v.name));
+                            }
+                            console.log(v);
+                            return {
+                                name: v.name,
+                                isFile: v.isFile(),
+                                isDir: v.isDirectory(),
+                                size: stat.size,
+                                atime: stat.atimeMs,
+                                btime: stat.birthtimeMs,
+                                blocks: stat.blocks,
+                                blksize: stat.blksize,
+                                __url: (() => {
+                                    if (v.__o) {
+                                        return new URL(ctx.URL.origin + "/" + decodeURIComponent(ctx.URL.searchParams.get("path") ?? "")).toString();
+                                    }
+                                    else {
+                                        return new URL(ctx.URL.origin + decodeURIComponent(ctx.URL.searchParams.get("path") ?? "") + v.name).toString();
+                                    }
+                                })()
+                            };
+                        })
+                    });
+                }
+                else {
+                    ctx.body = {
+                        status: 404,
+                        data: null
+                    };
+                }
+                break;
+            case "underline_indexes":
+                let root = fs_extra_1.default.readdirSync("./mirrors/插件包", {
+                    encoding: "utf8",
+                    withFileTypes: true
+                });
+                let out = new Set();
+                for (const f of root) {
+                    console.log(f);
+                    let ff = fs_extra_1.default.readdirSync(path_1.default.join("./mirrors/插件包/", f.name), {
+                        encoding: "utf8",
+                        withFileTypes: true
+                    });
+                    for (const fff of ff.filter(v => v.isFile() && path_1.default.parse(v.name).ext == ".7z" && path_1.default.parse(v.name).name.split("_").length == 3)) {
+                        out.add(path_1.default.parse(fff.name).name + "_" + f.name);
+                    }
+                }
+                ctx.type = "text/plain";
+                ctx.body = Array.from(out).join("\r\n");
+        }
     }
-    ;
-    let ate = 0;
-    console.log("worker", "Current: ", uu);
-    console.log("worker", "Length: ", length);
-    let stream = fs_extra_1.default.createWriteStream(path_1.default.resolve('./', uu.path, uu.name));
-    ccd.on("data", (chunk) => {
-        ate += chunk.length;
-        stream.write(chunk);
-        console.log("worker", uu.name, ", ", (ate / 1024 / 1024).toFixed(2), "/", (length / 1024 / 1024).toFixed(2), "MiB, Speed: ", (chunk.length / 1024 / 1024).toFixed(2), "MiB/chunk");
-    });
-    await (new Promise(r => ccd.on("close", r)));
-    stream.close();
-    console.log(uu.name, "ok");
-}
-exports.worker = worker;
-function Sleep(ms) {
-    return new Promise(r => {
-        setTimeout(r, ms);
-    });
-}
+});
+const config = fs_extra_1.default.readJSONSync("./config.json");
+const server = app.listen(config.port, () => console.log(server));
